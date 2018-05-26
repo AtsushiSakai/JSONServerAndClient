65c890d4fa68f9972b85f02404de3978e83c800f
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-05-26 21:04:09 +0900

    add json_server.py

---
+++ python/json_server.py
@@ -0,0 +1,48 @@
+"""
+
+JSON Server in python
+
+author: Atsushi Sakai (@Atsushi_twi)
+
+"""
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib.parse import parse_qs, urlparse
+import json
+from datetime import datetime
+import math
+
+
+class MyHandler(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+
+        uri = self.path
+        ret = parse_qs(urlparse(uri).query, keep_blank_values=True)
+
+        content = {}
+        content["title"] = "Python JSON server"
+        content["time"] = datetime.now().strftime("%Y%m%d%H%M%S")
+        content["PI"] = math.pi
+        ret = json.dumps(content)
+
+        body = bytes(ret, 'utf-8')
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.send_header('Content-length', len(body))
+        self.end_headers()
+        self.wfile.write(body)
+
+
+def main():
+    host = 'localhost'
+    port = 8000
+    httpd = HTTPServer((host, port), MyHandler)
+    print('serving at port', port)
+    httpd.serve_forever()
+
+    print(__file__ + " start!!")
+
+
+if __name__ == '__main__':
+    main()

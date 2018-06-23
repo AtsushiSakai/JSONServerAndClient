86431e734c7833d90f88a57e4ead6752fb748354
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-06-23 15:25:11 +0900

    release json client

---
+++ julia/json_client.jl
@@ -0,0 +1,25 @@
+#
+# JSON client with Julia
+#
+# author: Atsushi Sakai
+#
+
+using Requests
+import JSON
+
+function main()
+    println(PROGRAM_FILE," start!!")
+
+    url = "http://localhost:8000"
+    json = Requests.json(Requests.get(url))
+    println(json)
+
+    println(PROGRAM_FILE," Done!!")
+end
+
+
+if length(PROGRAM_FILE)!=0 &&
+    contains(@__FILE__, PROGRAM_FILE)
+    @time main()
+end
+

b64e9eba431ee43f935e9f013aef3ff0110e35db
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-09-09 18:00:53 +0900

    add simple java client

--- java/JSONClient.java
+++ java/JSONClient.java
@@ -1,6 +1,20 @@
+/*
+ * JSON http clinet with java
+ *
+ * author Atsushi Sakai
+ */
+
+import JSONHttpLib.JSONRequest;
 
 public class JSONClient{
    public static void main(String[] args){
      System.out.println("Hello World!!");
+
+     String strPostUrl = "http://localhost:8000";
+     String JSON = "{\"Type\":\"JSON client with Java\", \"test\":12345}";
+
+	 JSONHttpLib.JSONRequest jsonreq = new JSONHttpLib.JSONRequest();
+     String result = jsonreq.callPost(strPostUrl, JSON);
+     System.out.println(result);
    }
 }

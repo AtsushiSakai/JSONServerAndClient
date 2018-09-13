51671e0de195dfbb600c243be5a87900b9248354
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-09-13 21:28:36 +0900

    add code

--- java/JSONClient.java
+++ java/JSONClient.java
@@ -11,17 +11,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JSONClient{
-   public static void main(String[] args) throws Exception{
-     System.out.println("Hello World!!");
+	public static void main(String[] args) throws Exception{
+		System.out.println("Hello World!!");
 
-     String strPostUrl = "http://localhost:8000";
-     String JSON = "{\"Type\":\"JSON client with Java\", \"test\":12345}";
+		String strPostUrl = "http://localhost:8000";
+		String JSON = "{\"Type\":\"JSON client with Java\", \"test\":12345}";
 
-	 JSONHttpLib.JSONRequest jsonreq = new JSONHttpLib.JSONRequest();
-     String res = jsonreq.callPost(strPostUrl, JSON);
+		JSONHttpLib.JSONRequest jsonreq = new JSONHttpLib.JSONRequest();
+		String res = jsonreq.callPost(strPostUrl, JSON);
 
 		Map<String, Object> res_map = JSONHttpLib.JSONParser.ConvertjsonToMap(res);
 		System.out.println(res_map);
-	System.out.println(res_map.get("time"));
-   }
+	}
 }
--- java/JSONServer.java
+++ java/JSONServer.java
@@ -1,6 +1,55 @@
+/*
+ * JSON http clinet with java
+ *
+ * author Atsushi Sakai
+ */
 
-public class JSONServer{
-   public static void main(String[] args){
-     System.out.println("JSON Server");
-   }
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class JSONServer {
+
+    public static void main(String[] args) throws Exception {
+
+    	System.out.println("JSON Server");
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+    		System.out.println("request");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(t.getRequestBody(), StandardCharsets.UTF_8));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
+
+            String response = "{\"Name\":\"request_inline\"}";
+
+			System.out.println("response");
+    		System.out.println(response);
+
+            t.sendResponseHeaders(200, response.length());//200 is ok in Http status
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
 }

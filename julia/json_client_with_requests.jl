67f9b5f68e28d27b92640aa60cfcd5ef027accac
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-11-09 11:43:39 +0900

    released json request code with HTTP

--- julia/json_client_with_requests.jl
+++ julia/json_client_with_requests.jl
@@ -4,21 +4,18 @@
 # author: Atsushi Sakai
 #
 
-using HTTP
-using JSON
+using Requests
 
 function main()
+
 	req = Dict("Name" => "julia json request")
 	println("request:")
 	println(req)
 
     url = "http://localhost:8000"
-  	r = HTTP.request("POST", url,
-               ["Content-Type" => "application/json"],
-               JSON.json(req))
-
+    res = Requests.json(Requests.post(url; json = req))
     println("response:")
-  	println(JSON.parse(String(r.body)))
+    println(res)
 
     println(PROGRAM_FILE," Done!!")
 end

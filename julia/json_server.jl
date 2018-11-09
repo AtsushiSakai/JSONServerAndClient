e935b40067b48c117f768a19ebf4e445a3ec774b
Atsushi Sakai <asakai.amsl+github@gmail.com>
2018-11-09 11:27:30 +0900

    released julia json_server with HTTP instead of Httpserver

--- julia/json_server.jl
+++ julia/json_server.jl
@@ -4,38 +4,35 @@
 # author: Atsushi Sakai
 #
 
-using HttpServer
+using HTTP
 using JSON
+using Dates
 
-function response_json(req)
-	println("json requested!!")
-
-	# Parse input json 
-    jreq=JSON.parse(String(req.data))
-	println("request:")
-	println(jreq)
-
-    jres = Dict("title"=>"Julia JSON server", "PI" => pi)
-    jres["time"] = Dates.format(Dates.now(), "yyyymmddHHMMSS")
-	println("response:")
-	println(jres)
-
-    return JSON.json(jres)
+function show_http_request(http)
+	@show http.message
+	@show HTTP.header(http, "Content-Type")
+	while !eof(http)
+		println("body data: ", String(readavailable(http)))
+	end
 end
-
 
 function main()
     println(PROGRAM_FILE," start!!")
 
-    http = HttpHandler() do req::Request, res::Response
-        Response(response_json(req))
-    end
-
-    server = Server(http)
-    host = IPv4(127,0,0,1) # localhost
-    port = 8000
-    run(server, host=host, port=port)
+	ip = "127.0.0.1"
+	port = 8000
     println("serving at port", port)
+
+	HTTP.listen(ip, port) do http::HTTP.Stream
+
+		show_http_request(http)
+
+		#response json
+	    jdic = Dict("title"=>"Julia JSON server", "PI" => pi)
+    	jdic["time"] = Dates.format(Dates.now(), "yyyymmddHHMMSS")
+
+		write(http, JSON.json(jdic))
+	end
 
     println(PROGRAM_FILE," Done!!")
 end
